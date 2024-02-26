8f3a0597bd8c3047a963411e936759f749c19d23
Trần Thành Đạt <tthanhdat.pb@gmail.com>
2024-02-26 15:48:41 +0700

    some init msg

---
+++ src/main/java/com/rs/retailstore/controller/CustomerGreetingController.java
@@ -0,0 +1,24 @@
+package com.rs.retailstore.controller;
+
+import com.rs.retailstore.model.Greeting;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("/v1")
+public class CustomerGreetingController {
+
+    private static final String greetingTemplate = " Hello, %s %s";
+    private final AtomicLong counter = new AtomicLong();
+    @GetMapping("/greeting")
+
+    public <value> Greeting greeting(RequestParam(value = "gender" defaultValue = "0") boolean gender) {
+        return Greeting.builder()
+                .id(counter.incrementAndGet())
+                .content()
+                .build();
+    }
+}
+
